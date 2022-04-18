1e93b4b472a7856fe52286c57458851dccd1a3e0
connyycai <conny.cai@utexas.edu>
2022-04-18 15:29:46 -0500

    Update homework5.js

--- homework5.js
+++ homework5.js
@@ -1,28 +1,37 @@
+// Homework for Week 6
+
+// Pt.1 reverse
+
 function reverse(string) {
     var string2 = ""
     for (var i = string.length - 1; i >= 0; i--) {
-        string2.concat(string[i]);      
+        string2 += string[i];      
     }
     return string2;
 }
 
-reverse("UT Austin")
-reverse("TPEO")
-reverse("Pecan Pie")
+console.log(reverse("UT Austin"))
+console.log(reverse("TPEO"))
+console.log(reverse("Pecan Pie"))
+
+// Pt.2 numVowels
 
 function numVowels(string) {
     var count = 0
+    string = string.toLowerCase()
     for (var i = 0; i < string.length; i++) {
-        if (string[i] == "a") or (string[i] == "e") or (string[i] == "i") or (string[i] == "o") or (string[i] == "u") {
+        if (string[i] == "a" || string[i] === "e" || string[i] === "i" || string[i] === "o" || string[i] === "u") {
             count += 1;
         }
     }
     return count;
 }
 
-numVowels("UT Austin")
-numVowels("TPEO")
-numVowels("Pecan Pie")
+console.log(numVowels("UT Austin")) // 4
+console.log(numVowels("TPEO")) // 2
+console.log(numVowels("Pecan Pie"))  // 4
+
+// Pt.3 numStringsShorterThanLength
 
 function numStringsShorterThanLength(strings, length) {
     var count = 0
@@ -34,6 +43,6 @@ function numStringsShorterThanLength(strings, length) {
     return count
 }
 
-numStringsShorterThanLength(["pig", "goat", "lion", "hare"], 3)
-numStringsShorterThanLength(["fourteen", "six", "eleven", "twelve"], 5)
-numStringsShorterThanLength(["jester", "duren", "littlefield", "san jacinto"], 6)
+console.log(numStringsShorterThanLength(["pig", "goat", "lion", "hare"], 3)) // 0 
+console.log(numStringsShorterThanLength(["fourteen", "six", "eleven", "twelve"], 5)) // 1
+console.log(numStringsShorterThanLength(["jester", "duren", "field", "san jacinto"], 6)) // 2
