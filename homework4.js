9b91706995d450fdffb5b841cb6bff88f6a43bf6
connyycai <conny.cai@utexas.edu>
2022-04-18 15:12:57 -0500

    Update homework4.js

--- homework4.js
+++ homework4.js
@@ -1,3 +1,7 @@
+// Homework for Week 5
+
+// Pt.1 Car
+
 function Car(model, year, color, mpg) {
     this.model = model;
     this.year = year;
@@ -5,21 +9,23 @@ function Car(model, year, color, mpg) {
     this.mpg = mpg;
 }
 
-car1 = new Car("A-Class", 2021, "Black", 25)
-car2 = new Car("Aviator", 2022, "Black", 18)
-car3 = new Car("bZ4X", 2023, "Silver", 40)
+var car1 = new Car("A-Class", 2021, "Black", 25)
+var car2 = new Car("Aviator", 2022, "Black", 18)
+var car3 = new Car("bZ4X", 2023, "Silver", 40)
 
 console.log(car1.mpg)
 console.log(car2.mpg)
 console.log(car3.mpg)
 
+// Pt.2 Vehicle
+
 function Vehicle(car) {
     this.car = car;
 }
 
-vehicle1 = new Vehicle(car1);
-vehicle2 = new Vehicle(car2);
-vehicle3 = new Vehicle(car3);
+var vehicle1 = new Vehicle(car1);
+var vehicle2 = new Vehicle(car2);
+var vehicle3 = new Vehicle(car3);
 
 console.log(vehicle1.car.mpg);
 console.log(vehicle2.car.mpg);
