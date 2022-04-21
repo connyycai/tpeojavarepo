e1d089d96d4f1f9c2b63cbfeb0f8a400e1ae286c
connyycai <conny.cai@utexas.edu>
2022-04-21 18:21:56 -0500

    Update homework6.js

--- homework6.js
+++ homework6.js
@@ -2,37 +2,50 @@
 
 // Pt.1 Car
 
-var Car = []
-for (var i = 0; i < 10; i++) {
-    for (var j = 0; j < 10; j++) {
-        Car[i][j] = {
-            model: 6,
-            color: "black",
-            year: 2002,
-            mpg: 60,
-            id: j + 1
-        }
-    }
+function Car(model, color, year, mpg, id) {
+    this.model = model;
+    this.color = color;
+    this.year = year;
+    this.mpg = mpg;
+    this.id = id;
 }
 
-console.log(Car);
+var Carlist = []
+for (var i = 0; i < 10; i++) {
+    var car = []
+    for (var j = 0; j < 10; j++) {
+        car.push(new Car("Tesla Model X", "black", 2021, 26.0, i * 10 + j + 1))
+    }
+    Carlist.push(car)
+}
+
+console.log(Carlist);
 
 // Pt.2 getCarsBetweenIds
 
-function getCarsBetweenIds(idMin, idMax) {
-    btwnCar = []
-    for (i = 0; i < 10; i++) {
-        Car.color = "blue";
-        for (j=0; j < 10; i++) {
-            if (Car[i][j].id >= idMin && Car[i][j].id <= idMax) {
-                btwnCar.push(Car[i][j]);
-            }
-            if (Car[i][j].id <= idMax) {
-                break
-            }
-        }
+function getCarsBetweenIds(idMin, idMax, Carlist) {
+    var btwnCar = []
+    var row = Math.floor(idMin / 10);
+    if (idMin % 10 == 0) {
+        row -- 
     }
-    return btwnCar
+    col = (idMin - 1) % 10
+    carId = Carlist[row][col].id;
+    while (carId <= idMax) {
+        var car = Carlist[row][col]
+        car.color = "blue"
+        btwnCar.push(car)
+        if (col % 9 == 0 && col != 0) {
+            col = 0
+            row ++
+        } else {
+            col ++
+        }
+        carId ++
+    }
+    return btwnCar;
 }
 
-console.log(getCarsBetweenIds(5,9))
+console.log(getCarsBetweenIds(5, 9, Carlist))
+console.log(getCarsBetweenIds(15, 100, Carlist))
+console.log(getCarsBetweenIds(15, 100, Carlist))
